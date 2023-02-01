9889e0a99245451575fd72baf54c0f332f57ea5a
Alex Dias <eng.alexdiasc@gmail.com>
2023-02-01 09:46:45 -0300

    first commit

---
+++ OOP/02 - Pilares OOP/01 - EstadoComportamento.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace OOP
+{
+    public class Pessoa
+    {
+        //Propriedades - Estados
+        public string Nome { get; set; }
+        public DateTime DataNascimento { get; set; }
+
+        //Comportamento - Geração de nova informação através da própria classe. Pode também alterar o estado da classe
+        public int CalcularIdade()
+        {
+            var dataAtual = DateTime.Now;
+            var idade = dataAtual.Year - DataNascimento.Year;
+
+            if (dataAtual < DataNascimento.AddYears(idade)) idade--;
+
+            return idade;
+        }
+    }
+}
